60ee1bd1c8406d9bac6718fda4d6cb2c610ceb25
Allek97 <ilyas.jsk@hotmail.ca>
2021-09-12 02:09:47 -0400

    finshing personal website project route

--- src/components/contentfulProjectsAbout.js/ContentfulAboutPersonalWebsite.js
+++ src/components/contentfulProjectsAbout.js/ContentfulAboutPersonalWebsite.js
@@ -29,7 +29,37 @@ const Container = styled.div`
   }
 `;
 
+const AboutImage = styled(GatsbyImage)`
+  width: 120%;
+  margin-top: 8rem;
+
+  @media only screen and (max-width: 65em) {
+    width: 100%;
+  }
+`;
+
+export const query = graphql`
+  {
+    contentfulProjects(title: { eq: "Personal Website" }) {
+      id
+      title
+      aboutImages {
+        gatsbyImageData(placeholder: TRACED_SVG, layout: FULL_WIDTH)
+      }
+    }
+  }
+`;
+
 const ContentfulAboutPersonalWebsite = () => {
+  const data = useStaticQuery(query);
+
+  const {
+    contentfulProjects: { aboutImages },
+  } = data;
+
+  const { gatsbyImageData } = aboutImages[0];
+
+  const personalWebsiteAllDevicesImage = getImage(gatsbyImageData);
   return (
     <Container>
       <p>
@@ -39,23 +69,43 @@ const ContentfulAboutPersonalWebsite = () => {
         reduces some complexities like routing and image performance.
       </p>
       <p>
-        I'v used Contentful which is headless CMS that allowed me to stores my
+        I've used Contentful which is headless CMS that allowed me to stores my
         images and most of the data related to the overview of my projects.
         GraphQL is used to communicate with contentful and fetch the necessary
         data through queries.
       </p>
       <p>
-        I'v used Figma to create/modify some images. For example, I'v used it to
-        add a frame with color gradients around projects thumbnails. It was also
-        used to animate the dashboard svg in the coding section of the homepage
-        and implement the devices mockups images like below. The mockups are
-        provided by Mockuuups Studio.
+        I've used Figma to create/modify some images. For example, I've used it
+        to add a frame with color gradients around projects thumbnails. It was
+        also used to animate the dashboard svg in the coding section of the
+        homepage and implement the devices mockups images like below. The
+        mockups are provided by Mockuuups Studio.
       </p>
-      <p>The biggest challenge</p>
-      {/* <AboutImage
-        image={stockersaAllDevicesImage}
-        alt="stockersa on all devices"
-      /> */}
+      <p>
+        In the homepage, I implemented a 3d globe using three js libraries. I've
+        used the examples and the documentation in{" "}
+        <a
+          href="https://github.com/vasturiano/three-globe"
+          target="_blank"
+          rel="noreferrer"
+        >
+          three-globe github library
+        </a>{" "}
+        to design my earth globe.
+      </p>
+      <p>
+        There is a contact section where you can download my Curriculum Vitae.
+        You can also use the form to contact me. I would be happy to respond as
+        fast as possible for an interview and any other opportunity.
+      </p>
+      <p>
+        My personal website is fully responsive, you can use it in any device
+        (runs better in chrome for now);
+      </p>
+      <AboutImage
+        image={personalWebsiteAllDevicesImage}
+        alt="personal website on all devices"
+      />
     </Container>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -198,11 +198,11 @@ export default function Index() {
                   <h1>Ilias Allek</h1>
                   <h3>Aspiring Software Developer</h3>
                   <p>
-                    I'v graduated from Montreal University on December 2020 with
-                    a Bachelor of Mathematics / minor in statistics. Before
+                    I've graduated from Montreal University on December 2020
+                    with a Bachelor of Mathematics / minor in statistics. Before
                     graduating, I was wondering about my future and what I will
                     do next, I didn't wanna pursue academics, It didn't seem fit
-                    to me. I'v decided that I will dedicate my time learning
+                    to me. I've decided that I will dedicate my time learning
                     programming. Over this time I learned bulding responsive
                     websites, full stack web applications based on React,Gatsby,
                     NodeJS and more. On my free time, I like to play soccer,
