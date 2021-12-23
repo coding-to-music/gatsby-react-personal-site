6bf4a5ae1c7d4b787fea48a3094110a8accd3e8a
Allek97 <ilyas.jsk@hotmail.ca>
2021-12-23 17:43:13 -0500

    small change

--- src/pages/index.js
+++ src/pages/index.js
@@ -293,15 +293,15 @@ export default function Index() {
                     <h1>Ilias Allek</h1>
                     <h3>Aspiring Software Developer</h3>
                     <p>
-                      I've graduated from Montreal University on December 2020
-                      with a Bachelor of Mathematics / minor in statistics.
-                      Before graduating, I wondered about my future and what I
-                      will do next, I didn't wanna pursue academics, It didn't
-                      seem fit to me. I've decided that I will dedicate my time
-                      learning programming. Over this time I learned building
-                      responsive websites, full stack web applications based on
-                      React,Gatsby, NodeJS and more. On my free time, I like to
-                      play soccer, exercice and hang out with friends.
+                      I've graduated from Montreal University with a Bachelor of
+                      Mathematics / minor in statistics. Before graduating, I
+                      wondered about my future and what I will do next, I didn't
+                      wanna pursue academics, It didn't seem fit to me. I've
+                      decided that I will dedicate my time learning programming.
+                      Over this time I learned building responsive websites,
+                      full stack web applications based on React,Gatsby, NodeJS
+                      and more. On my free time, I like to play soccer, exercice
+                      and hang out with friends.
                     </p>
                   </div>
                   <div ref={aboutStacksRef}>
